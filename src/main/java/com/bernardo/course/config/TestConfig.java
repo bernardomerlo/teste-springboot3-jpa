c8852937ad934ba43d4caba697668af83f03f2c5
Bernardo <bernardomerlo49@gmail.com>
2023-09-13 14:16:33 -0300

    Category

--- src/main/java/com/bernardo/course/config/TestConfig.java
+++ src/main/java/com/bernardo/course/config/TestConfig.java
@@ -8,35 +8,46 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.bernardo.course.entities.Category;
 import com.bernardo.course.entities.Order;
 import com.bernardo.course.entities.User;
 import com.bernardo.course.entities.enums.OrderStatus;
+import com.bernardo.course.repositories.CategoryRepository;
 import com.bernardo.course.repositories.OrderRepository;
 import com.bernardo.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
+public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepostory;
 
+	@Autowired
+	private CategoryRepository categoryRepostory;
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepostory.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		
+
 		Order o1 = new Order(null, Instant.parse("2024-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2024-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2024-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepostory.saveAll(Arrays.asList(o1,o2,o3));
-		
+		orderRepostory.saveAll(Arrays.asList(o1, o2, o3));
+
 	}
-	
-	
+
 }
